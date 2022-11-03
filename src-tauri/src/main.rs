2d81308cdee2d87664fd2976adbb6075bc028e8d
Brendan Osborne <brendan.r.osborne@gmail.com>
2022-11-03 19:55:31 +1000

    start stop wiresock process

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,14 @@
     windows_subsystem = "windows"
 )]
 
-use std::io::Write;
-use std::{path::PathBuf};
 use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+use std::process::Command;
 
 #[tauri::command]
 #[allow(non_snake_case)]
-fn create_wiresock_conf(
-    tunnelName: &str,
+fn enable_wiresock(
     privateKey: &str,
     interfaceAddress: &str,
     dns: &str,
@@ -19,26 +19,23 @@ fn create_wiresock_conf(
     allowedApps: &str,
     allowedIPs: &str,
 ) -> bool {
-    // format!("Hello, {}{}!", tunnelName, privateKey);
-    // write the conf file with the parameters
-    //let path: PathBuf = [home::home_dir(), "Tunnl.to", "tunnel.conf"].iter().collect();
-    let mut pathx = PathBuf::new();
+    // Write a wiresock config file to disk
 
+    // Get the users home directory
+    let mut pathx = PathBuf::new();
     match home::home_dir() {
         Some(path) => pathx.push(path),
-        None => println!("Impossible to get your home dir!"),
+        None => println!("Could not find the home dir"), // TODO: Do something better here
     }
 
+    // Create the path to the wiresock config file
     pathx.push("AppData");
     pathx.push("Local");
     pathx.push("Tunnl.to");
     pathx.push("tunnel.conf");
 
-    // let config_data: &str = ("[Interface]\nPrivateKey = {}\nAddress = {}", privateKey, interfaceAddress);
-
-    // fs::write(pathx, config_data).expect("Unable to write file");
-
-    let mut w = File::create(pathx).unwrap();
+    // Write the config file to disk
+    let mut w = File::create(&pathx).unwrap();
     writeln!(&mut w, "[Interface]").unwrap();
     writeln!(&mut w, "PrivateKey = {}", privateKey).unwrap();
     writeln!(&mut w, "Address = {}", interfaceAddress).unwrap();
@@ -52,13 +49,39 @@ fn create_wiresock_conf(
     writeln!(&mut w, "PersistentKeepalive = 25").unwrap();
     writeln!(&mut w, "AllowedApps = {}", allowedApps).unwrap();
 
+    // Enable Wiresock
+    // TODO: Get the proper path to where Wiresock was installed
+    let my_str = &pathx.into_os_string().into_string().unwrap();
+    let child = Command::new("C:/Program Files/Wiresock VPN Client/bin/wiresock-client.exe")
+        .arg("run")
+        .arg("-config")
+        .arg(my_str)
+        .arg("-log-level")
+        .arg("debug")
+        .spawn()
+        .expect("command failed to start");
+    let child_id = child.id();
+    println!("Child ID: {}", child_id);
 
+    return true;
+}
+
+#[tauri::command]
+fn disable_wiresock() -> bool {
+    // taskkill /F /IM wiresock-client.exe /T
+    Command::new("taskkill")
+        .arg("/F")
+        .arg("/IM")
+        .arg("wiresock-client.exe")
+        .arg("/T")
+        .spawn()
+        .expect("command failed to start");
     return true
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![create_wiresock_conf])
+        .invoke_handler(tauri::generate_handler![enable_wiresock, disable_wiresock])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
