2f80e7f9ac88659cd71c580ef55b72669d757d74
Brendan Osborne <brendan.r.osborne@gmail.com>
2022-11-04 12:16:38 +1000

    retrieving wiresock install path from registry

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,6 +7,7 @@ use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 use std::process::Command;
+extern crate winreg;
 
 #[tauri::command]
 #[allow(non_snake_case)]
@@ -22,20 +23,20 @@ fn enable_wiresock(
     // Write a wiresock config file to disk
 
     // Get the users home directory
-    let mut pathx = PathBuf::new();
+    let mut tunnel_config_path = PathBuf::new();
     match home::home_dir() {
-        Some(path) => pathx.push(path),
+        Some(path) => tunnel_config_path.push(path),
         None => println!("Could not find the home dir"), // TODO: Do something better here
     }
 
     // Create the path to the wiresock config file
-    pathx.push("AppData");
-    pathx.push("Local");
-    pathx.push("Tunnl.to");
-    pathx.push("tunnel.conf");
+    tunnel_config_path.push("AppData");
+    tunnel_config_path.push("Local");
+    tunnel_config_path.push("Tunnl.to");
+    tunnel_config_path.push("tunnel.conf");
 
     // Write the config file to disk
-    let mut w = File::create(&pathx).unwrap();
+    let mut w = File::create(&tunnel_config_path).unwrap();
     writeln!(&mut w, "[Interface]").unwrap();
     writeln!(&mut w, "PrivateKey = {}", privateKey).unwrap();
     writeln!(&mut w, "Address = {}", interfaceAddress).unwrap();
@@ -49,13 +50,27 @@ fn enable_wiresock(
     writeln!(&mut w, "PersistentKeepalive = 25").unwrap();
     writeln!(&mut w, "AllowedApps = {}", allowedApps).unwrap();
 
+    // Get the Wiresock install location
+    use winreg::enums::{HKEY_LOCAL_MACHINE, KEY_READ};
+    let hklm = winreg::RegKey::predef(HKEY_LOCAL_MACHINE);
+    let subkey = hklm
+        .open_subkey_with_flags(r#"SOFTWARE\NTKernelResources\WinpkFilterForVPNClient"#, KEY_READ)
+        .expect("Failed to open subkey");
+    let mut wiresock_location: String = subkey
+        .get_value("InstallLocation")
+        .expect("Failed to read product name");
+    // Complete the path to the wiresock executable
+    let exe: &str = "/bin/wiresock-client.exe";
+    wiresock_location.push_str(exe);
+
+    // Command::new args require strings so prepare path to Wiresock config file
+    let wiresock_config_path = &tunnel_config_path.into_os_string().into_string().unwrap();
+
     // Enable Wiresock
-    // TODO: Get the proper path to where Wiresock was installed
-    let my_str = &pathx.into_os_string().into_string().unwrap();
-    let child = Command::new("C:/Program Files/Wiresock VPN Client/bin/wiresock-client.exe")
+    let child = Command::new(wiresock_location)
         .arg("run")
         .arg("-config")
-        .arg(my_str)
+        .arg(wiresock_config_path)
         .arg("-log-level")
         .arg("debug")
         .spawn()
@@ -75,7 +90,7 @@ fn disable_wiresock() -> bool {
         .arg("/T")
         .spawn()
         .expect("command failed to start");
-    return true
+    return true;
 }
 
 fn main() {
