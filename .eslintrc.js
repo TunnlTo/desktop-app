4ba2fa8984c47bc9cd69733aa9fa23b5f9a09a96
Brendan Osborne <brendan.r.osborne@gmail.com>
2023-10-24 14:21:31 +1000

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: 'standard',
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+  },
+  plugins: [
+    'html'
+  ]
+}
--- dist/js/tunnel.js
+++ dist/js/tunnel.js
@@ -1,29 +1,30 @@
 // Define the object
-function Tunnel(
-  name,
-  privateKey,
-  interfaceAddress,
-  dns,
-  publicKey,
-  presharedKey,
-  endpoint,
-  allowedApps,
-  disallowedApps,
-  allowedIPs,
-  disallowedIPs,
-  mtu) {
-  this.name = name
-  this.privateKey = privateKey
-  this.interfaceAddress = interfaceAddress
-  this.dns = dns
-  this.publicKey = publicKey
-  this.presharedKey = presharedKey
-  this.endpoint = endpoint
-  this.allowedApps = allowedApps
-  this.disallowedApps = disallowedApps
-  this.allowedIPs = allowedIPs
-  this.disallowedIPs = disallowedIPs
-  this.mtu = mtu
+class Tunnel {
+  constructor (name,
+    privateKey,
+    interfaceAddress,
+    dns,
+    publicKey,
+    presharedKey,
+    endpoint,
+    allowedApps,
+    disallowedApps,
+    allowedIPs,
+    disallowedIPs,
+    mtu) {
+    this.name = name
+    this.privateKey = privateKey
+    this.interfaceAddress = interfaceAddress
+    this.dns = dns
+    this.publicKey = publicKey
+    this.presharedKey = presharedKey
+    this.endpoint = endpoint
+    this.allowedApps = allowedApps
+    this.disallowedApps = disallowedApps
+    this.allowedIPs = allowedIPs
+    this.disallowedIPs = disallowedIPs
+    this.mtu = mtu
+  }
 }
 
 // Export the object
