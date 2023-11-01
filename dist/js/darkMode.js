8de5989aaca97e899fa40320a87045e15916fe3e
Brendan Osborne <brendan.r.osborne@gmail.com>
2023-11-01 17:38:38 +1000

    refactor wip

--- dist/js/darkMode.js
+++
@@ -1,4 +0,0 @@
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  console.log('Enabling dark mode')
-  document.documentElement.setAttribute('data-bs-theme', 'dark')
-}
---
+++ dist/js/globals.js
@@ -0,0 +1,27 @@
+// Show a Toast message
+function showToast (toastElement, toastMessageElement, message) {
+  toastMessageElement.innerHTML = message
+  toastElement.classList.add('show')
+  setTimeout(function () { closeToast(toastElement) }, 3000)
+}
+
+// Close the toast
+function closeToast (toastElement) {
+  toastElement.classList.remove('show')
+}
+
+// Set light or dark mode
+function setDarkMode () {
+  const darkMode = localStorage.getItem('darkMode') === 'true'
+  if (darkMode) {
+    document.body.classList.add('dark-mode')
+    document.body.classList.remove('light-mode')
+    document.body.classList.remove('hidden')
+  } else {
+    document.body.classList.add('light-mode')
+    document.body.classList.remove('dark-mode')
+    document.body.classList.remove('hidden')
+  }
+}
+
+export { showToast, closeToast, setDarkMode }
